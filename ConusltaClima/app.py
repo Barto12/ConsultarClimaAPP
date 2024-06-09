cf3ee5da86489d8d42be3a1abc8025a18e99ca22
Barto12 <nzgraphics.studio@gmail.com>
2024-06-09 13:17:25 -0600

    Add files via upload
    Primera version del programa

---
+++ ConusltaClima/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+API_KEY = '44af5bea62af4f6e0311f09495ca516e'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    weather = None
+    if request.method == 'POST':
+        city = request.form['city']
+        weather = get_weather(city)
+    return render_template('index.html', weather=weather)
+
+def get_weather(city):
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        weather = {
+            'city': data['name'],
+            'temperature': data['main']['temp'],
+            'description': data['weather'][0]['description'],
+            'icon': data['weather'][0]['icon']
+        }
+        return weather
+    else:
+        return None
+
+if __name__ == '__main__':
+    app.run(debug=True)
